f6ee673e2da21305407589145be1ce91fab7018b
Julie - Mathman <casperschorr06@gmail.com>
2025-03-15 16:49:35 -0400

    remove unused files and components, including styles, types, and theme management

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/NodeChart.css';
-import '../styles/wiki-graph.css';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
--- types/NodeTypes.ts
+++
@@ -1,9 +0,0 @@
-export interface NodeData {
-  id: string;
-  title: string;
-  path: string;
-  preview: string;
-  links: string[];
-  x: number;
-  y: number;
-}
